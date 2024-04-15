40d8801fe062445f4b6de8be3763b1528ebb17a0
Thanh Trung <71776470+gnurt2041@users.noreply.github.com>
2024-04-15 14:26:50 +0900

    Update dynamic.py

--- util/dynamic.py
+++ util/dynamic.py
@@ -20,9 +20,9 @@ def separate_users(args, dict_users):
 def merge_users(dict_users, new_users, args, stage = 1):
     
     if stage == 1:
-        num_new_users = np.round(args.num_new_users*args.stage_ratio)
+        num_new_users = int(np.round(args.num_new_users*args.stage_ratio))
     elif stage == 2:
-        num_new_users = np.ceil(args.num_new_users*(1-args.stage_ratio))
+        num_new_users = int(np.ceil(args.num_new_users*(1-args.stage_ratio)))
                             
     for _ in range(num_new_users):
           
